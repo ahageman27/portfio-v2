cbf899f8ad21868abafba06d996f1c572a22939e
Alex Hageman <ahageman27@gmail.com>
2022-09-08 12:30:16 -0600

    download resume link

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 This is the portfolio of Alex Hageman built using react.
 https://ahageman27.github.io/portfolio-v2/
 
-![Screenshot of Portfoio]("./src/images/screenshot.jpg")
+![Screenshot of Portfoio]()
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,106 +4,108 @@ import Box from "@mui/material/Box";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
-
+import { BrowserRouter as Router, Link } from "react-router-dom";
 const Resume = (props) => {
   return (
-    <Container className="d-flex flex-wrap justify-content-center">
-      <Typography
-        variant="h3"
-        noWrap
-        component="a"
-        href="/"
-        sx={{
-          m: 5,
-          display: "flex",
-          justifyContent: "center",
-          fontFamily: "monospace",
-          fontWeight: 700,
-          letterSpacing: ".3rem",
-          color: "inherit",
-          textDecoration: "none",
-        }}
-      >
-        RESUME
-      </Typography>
-      <Typography
-        variant="h6"
-        noWrap
-        component="a"
-        href="/"
-        sx={{
-          m: 5,
-          display: "flex",
-          justifyContent: "start",
-          fontFamily: "monospace",
-          fontWeight: 700,
-          letterSpacing: ".3rem",
-          color: "inherit",
-          textDecoration: "none",
-        }}
-      >
-        PROFICIENCIES
-      </Typography>
-      <Box
-        sx={{
-          display: "flexrow",
-          justifyContent: "center",
-          width: "100%",
-          maxWidth: 360,
-          bgcolor: "primary.secondary",
-        }}
-      >
-        <List sx={{ display: "flexrow", justifyContent: "center" }}>
-          <ListItem disablePadding>
-            <ListItemText primary="JavaScript" />
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemText primary="Python" />
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemText primary="HTML & CSS" />
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemText primary="Object Oriented Programming" />
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemText primary="REACT" />
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemText primary="NodeJS" />
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemText primary="SQL" />
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemText primary="MongoDB" />
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemText primary="GraphQL" />
-          </ListItem>
-        </List>
-      </Box>
-      <Typography
-        variant="h6"
-        noWrap
-        component="a"
-        href="/"
-        sx={{
-          m: 5,
-          display: "flex",
-          justifyContent: "center",
-          fontFamily: "monospace",
-          fontWeight: 700,
-          letterSpacing: ".3rem",
-          color: "inherit",
-          textDecoration: "none",
-        }}
-      >
-        <a href="../resume/Resume2022.pdf" download>
-          DOWNLOAD
-        </a>
-      </Typography>
-    </Container>
+    <Router>
+      <Container className="d-flex flex-wrap justify-content-center">
+        <Typography
+          variant="h3"
+          noWrap
+          component="a"
+          href="/"
+          sx={{
+            m: 5,
+            display: "flex",
+            justifyContent: "center",
+            fontFamily: "monospace",
+            fontWeight: 700,
+            letterSpacing: ".3rem",
+            color: "inherit",
+            textDecoration: "none",
+          }}
+        >
+          RESUME
+        </Typography>
+        <Typography
+          variant="h6"
+          noWrap
+          component="a"
+          href="/"
+          sx={{
+            m: 5,
+            display: "flex",
+            justifyContent: "start",
+            fontFamily: "monospace",
+            fontWeight: 700,
+            letterSpacing: ".3rem",
+            color: "inherit",
+            textDecoration: "none",
+          }}
+        >
+          PROFICIENCIES
+        </Typography>
+        <Box
+          sx={{
+            display: "flexrow",
+            justifyContent: "center",
+            width: "100%",
+            maxWidth: 360,
+            bgcolor: "primary.secondary",
+          }}
+        >
+          <List sx={{ display: "flexrow", justifyContent: "center" }}>
+            <ListItem disablePadding>
+              <ListItemText primary="JavaScript" />
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemText primary="Python" />
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemText primary="HTML & CSS" />
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemText primary="Object Oriented Programming" />
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemText primary="REACT" />
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemText primary="NodeJS" />
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemText primary="SQL" />
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemText primary="MongoDB" />
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemText primary="GraphQL" />
+            </ListItem>
+          </List>
+        </Box>
+        <Typography
+          variant="h6"
+          noWrap
+          component="a"
+          href="/"
+          sx={{
+            m: 5,
+            display: "flex",
+            justifyContent: "center",
+            fontFamily: "monospace",
+            fontWeight: 700,
+            letterSpacing: ".3rem",
+            color: "inherit",
+            textDecoration: "none",
+          }}
+        >
+          <a href="../../public/resume/AlexHagemanResume.pdf" target="_blank" download>
+            Download
+          </a>
+        </Typography>
+      </Container>
+    </Router>
   );
 };
 
